da7ae95bc3044dc1257c5f4762fb2ee33a393ec2
Abhey Singh <abheys172@gmail.com>
2025-07-26 09:20:04 +0530

    fixed server

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:5000"
+  baseURL: "https://instaclone-bw02.onrender.com"
 });
 
 // axios.interceptors.response.use(
